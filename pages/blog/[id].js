6fd3e6f1e27151a9ae5a6af1b3cc3e1dd56a9013
Eli <pleanbean@gmail.com>
2020-01-04 21:16:44 +0100

    exploration of async context object

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,14 +1,29 @@
 import {useRouter} from "next/router";
 import Layout from "../../components/layout";
+import fetch from "isomorphic-unfetch";
 
-const Post = () => {
+const Post = props => {
   const router = useRouter();
+  const {show} = props;
+
+  const summary = show.summary.replace(/<[/]?[pb]>/g, "");
+
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>post content</p>
+      <h1>{show.name}</h1>
+      <p>{summary}</p>
+      <img src={show.image.medium} />
     </Layout>
   );
+};
+
+Post.getInitialProps = async context => {
+  const {id} = context.query;
+
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  return {show};
 };
 
 export default Post;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -13,7 +13,7 @@ const Blog = props => (
 Blog.getInitialProps = async () => {
   const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
   const data = await res.json();
-  console.log(`Show data fetched. Count: ${data.length}`);
+
   return {
     shows: data.map(entry => entry.show) || []
   };
