5a4c3e37454d03c489e046745b90761342771ac6
Eli <pleanbean@gmail.com>
2020-01-11 20:40:22 +0100

    Add similar artist fetching using axios

--- components/lastfm-viz/artistFetch.js
+++ components/lastfm-viz/artistFetch.js
@@ -2,13 +2,11 @@ import fetch from "isomorphic-unfetch";
 import useSWR from "swr";
 import { APIKey } from "../../secrets.json";
 
-const fetchUrl = `https://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=pleanut&api_key=${APIKey}&format=json`;
-
-const fetcher = url => {
-  return fetch(url).then(r => r.json());
-};
-
 const useArtistFetch = () => {
+  const fetchUrl = `https://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=pleanut&api_key=${APIKey}&format=json`;
+  const fetcher = url => {
+    return fetch(url).then(r => r.json());
+  };
   const { data, error } = useSWR(fetchUrl, fetcher);
 
   const fetched = {
