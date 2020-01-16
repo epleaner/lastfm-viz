343a730dd5e9635cd003abea59fd90373ca5366c
Eli <pleanbean@gmail.com>
2020-01-16 13:07:15 +0100

    formatting

--- components/lastfm-viz/artistSearchPanel.js
+++ components/lastfm-viz/artistSearchPanel.js
@@ -3,7 +3,9 @@ import useArtistFetch from "./artistFetch";
 
 const ArtistSearchPanel = props => {
   const { artists, loading, error } = useArtistFetch();
+
   let content;
+
   if (loading) content = <span>Loading...</span>;
   else if (error) content = <span>Failed to fetch: ${error}</span>;
   else content = <ArtistList artists={artists} {...props} />;
