64f078a2b3de47fbfa975059e48235645f1f18c0
Eli <pleanbean@gmail.com>
2020-01-16 23:21:07 +0100

    dump in global styling (for now...)

--- pages/lastfm-viz/index.js
+++ pages/lastfm-viz/index.js
@@ -47,6 +47,77 @@ const LastFMPage = () => {
         grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
         align-items: flex-start;
       `}</style>
+
+      <style global jsx>{`
+        /*** typography ***/
+        html {
+          font-size: 1px;
+        }
+
+        body {
+          font-size: 16rem;
+          font-weight: 400;
+          line-height: 3;
+          color: #000;
+          margin: 5vh 5vw;
+        }
+
+        /*** resets ***/
+
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+        a {
+          text-decoration: none;
+          color: inherit;
+          cursor: pointer;
+        }
+        button {
+          background-color: transparent;
+          color: inherit;
+          border-width: 0;
+          padding: 0;
+          cursor: pointer;
+        }
+        figure {
+          margin: 0;
+        }
+        input::-moz-focus-inner {
+          border: 0;
+          padding: 0;
+          margin: 0;
+        }
+        ul,
+        ol,
+        dd {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+        }
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          margin: 0;
+          font-size: inherit;
+          font-weight: inherit;
+        }
+        p {
+          margin: 0;
+        }
+        cite {
+          font-style: normal;
+        }
+        fieldset {
+          border-width: 0;
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
     </section>
   );
 };
