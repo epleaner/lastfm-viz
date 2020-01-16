04c9ec15cb587c541f2e2d3435c996512375ae2e
Eli <pleanbean@gmail.com>
2020-01-16 13:59:45 +0100

    albums panel uses async panel component

--- components/lastfm-viz/albumsPanel.js
+++ components/lastfm-viz/albumsPanel.js
@@ -1,26 +1,20 @@
 import AlbumsList from "./albumsList";
-import EmptyPanel from "./emptyPanel";
-import ErrorPanel from "./errorPanel";
+import AsyncPanel from "./asyncPanel";
 
 const AlbumsPanel = props => {
-  const { data } = props;
-
-  if (!data) {
-    return <EmptyPanel />;
-  }
-
-  if (data.error) {
-    return <ErrorPanel error={data.error} />;
-  }
-
-  const sortedData = data.data.sort(
-    ({ playcountA }, { playcountB }) => playcountB >= playcountA
-  );
+  const sortByPlaycount = (
+    { playcount: playcountA },
+    { playcount: playcountB }
+  ) => playcountB >= playcountA;
 
   return (
-    <section>
-      <AlbumsList albums={sortedData} {...props} />
-    </section>
+    <AsyncPanel
+      {...props}
+      render={(data, otherProps) => {
+        const sortedData = data.sort(sortByPlaycount);
+        return <AlbumsList albums={sortedData} {...otherProps} />;
+      }}
+    />
   );
 };
 
