2d453bf1320cb3a146fe7f55f23c1f35055c46b5
Eli <pleanbean@gmail.com>
2020-01-16 15:20:49 +0100

    Refactor lastfm API interface to support different resource fetches

--- api/lastfm.js
+++ api/lastfm.js
@@ -1,15 +1,31 @@
 import axios from "axios";
 import { APIKey } from "../secrets.json";
 
-const createFetcher = (url, group, subgroup) => async (name, mbid) => {
-  const { data, error } = await axios.get(url, {
-    params: {
-      artist: name,
-      autocorrect: 1,
-      api_key: APIKey,
-      format: "json"
-    }
-  });
+const createFetcher = ({ url, group, subgroup, resource }) => async name => {
+  const defaultParams = {
+    autocorrect: 1,
+    api_key: APIKey,
+    format: "json"
+  };
+
+  let params;
+
+  switch (resource) {
+    case "artist":
+      params = {
+        artist: name,
+        ...defaultParams
+      };
+      break;
+    case "user":
+      params = {
+        user: name,
+        ...defaultParams
+      };
+      break;
+  }
+
+  const { data, error } = await axios.get(url, { params });
 
   if (error || data.error) {
     return {
@@ -25,14 +41,23 @@ const createFetcher = (url, group, subgroup) => async (name, mbid) => {
   };
 };
 
-export const fetchTopAlbums = createFetcher(
-  "https://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums",
-  "topalbums",
-  "album"
-);
+export const fetchTopAlbums = createFetcher({
+  url: "https://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums",
+  group: "topalbums",
+  subgroup: "album",
+  resource: "artist"
+});
 
-export const fetchSimilarArtists = createFetcher(
-  "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar",
-  "similarartists",
-  "artist"
-);
+export const fetchSimilarArtists = createFetcher({
+  url: "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar",
+  group: "similarartists",
+  subgroup: "artist",
+  resource: "artist"
+});
+
+export const fetchUsersWeeklyCharts = createFetcher({
+  url: "https://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart",
+  group: "weeklyartistchart",
+  subgroup: "artist",
+  resource: "user"
+});
