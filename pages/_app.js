e3c8ccb16a7f8b61deff0a0495f10295fdead12f
Eli <pleanbean@gmail.com>
2020-01-17 10:57:59 +0100

    Updates Nextjs to 9.2, thus allowing for importing global stylesheet

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import "../assets/styles/resets/reset.local.css";
+import "../assets/styles/resets/typography.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
